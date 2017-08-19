a233b57d4a3097602f62c69433bc0b6f57fda918
Niels van Dam <nielsvdam@hotmail.com>
2017-08-19 02:52:06 +0200

    Movement en raycast yo

---
+++ Assets/Scripts/EnemyOrigami.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+public class EnemyOrigami : MonoBehaviour
+{
+    public int attack;
+    public int health;
+    public int attackSpeed;
+    public CreatureType type;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
