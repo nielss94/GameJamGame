e71b2c8974185aed4181f6d55f5caa84f654a4e6
nielss94 <nielsvdam@hotmail.com>
2017-08-20 02:37:41 +0200

    Scenes scales menu

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour {
+
+    public List<GameObject> levels;
+    public Button levelButton;
+    
+	void Start () {
+        GetLevels();
+	}
+	
+
+    void GetLevels()
+    {
+        foreach (GameObject go in levels)
+        {
+            Button b = Instantiate(levelButton) as Button;
+            b.transform.SetParent(transform);
+            b.transform.GetChild(0).GetComponent<Text>().text = go.GetComponent<Level>().levelName + " \n\n  " + go.GetComponent<Level>().papers.ToString() + " papers";
+            b.onClick.AddListener(() => LoadLevel(go));
+        }
+    }
+
+    void LoadLevel(GameObject level)
+    {
+        GameController.instance.StartCoroutine(GameController.instance.LoadLevel(level));
+    }
+}
