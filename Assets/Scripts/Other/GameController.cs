0fb2c7ea58c33602735a871e47c447ae3add8669
nielss94 <nielsvdam@hotmail.com>
2017-08-20 21:08:32 +0200

    Stuff

--- Assets/Scripts/Other/GameController.cs
+++ Assets/Scripts/Other/GameController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Enums;
 
 public class GameController : MonoBehaviour {
 
     public static GameController instance;
+    public AudioClip propClip;
+    private AudioSource audioSource;
+    public GameObject endScreen;
 
 	// Use this for initialization
 	void Start () {
+        audioSource = GetComponent<AudioSource>();
         instance = this;
         DontDestroyOnLoad(gameObject);
     }
@@ -20,7 +25,13 @@ public class GameController : MonoBehaviour {
         GameObject[] friendlies = GameObject.FindGameObjectsWithTag("Friendly");
         if(friendlies.Length <= 0 && GameObject.Find("PaperPile").GetComponent<PaperPile>().folding == false && GameObject.Find("PaperPile").GetComponent<PaperPile>().paperStock <= 0){
             Debug.Log("Game over!");
+            GameObject go = Instantiate(endScreen) as GameObject;
+            go.transform.parent = transform;
+            go.GetComponent<ScreenCanvas>().customText.text = "YOU LOSE!";
+            go.GetComponent<ScreenCanvas>().optionButton.transform.GetChild(0).GetComponent<Text>().text = "Try again";
+            go.GetComponent<ScreenCanvas>().optionButton.onClick.AddListener(() => StartCoroutine(LoadLevel(LevelController.instance.currentStage)));
         }
+       
     }
 
     public IEnumerator LoadLevel(GameObject level)
@@ -43,10 +54,10 @@ public class GameController : MonoBehaviour {
 
     public IEnumerator SpawnOrigami(Origami origami, GameObject lane, GameObject hands)
     {
-        Debug.Log("AOOO?");
+
         GameObject go = Instantiate(hands, new Vector3(Camera.main.transform.position.x, hands.transform.position.y, Camera.main.transform.position.z + 7.5f), hands.transform.rotation) as GameObject;
+        audioSource.PlayOneShot(propClip);
         yield return new WaitForSeconds(2.5f);
-        Debug.Log("AOOO2?");
         Destroy(go);
         GameObject.Find("Main Camera").GetComponent<Player>().SpawnOrigami(origami, lane, LevelController.instance.stageNumber);
     }
